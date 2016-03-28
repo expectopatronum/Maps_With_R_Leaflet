67e42bb09aee3ed537d28e0ca7d984e877b994f1
expectopatronum <veroamilbe@gmail.com>
2016-03-28 07:46:32 +0200

    fixes 'cannot open ...' on capitals.RDS bug

--- Basic_Map/initLesson.R
+++ Basic_Map/initLesson.R
@@ -3,9 +3,12 @@
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
 
-# Source utilities.R
-# source(file.path(find.package("swirl"), "Courses", "Writing_swirl_Courses", "R", "utilities.R"))
 
-capitals <- readRDS("capitals.RDS")
+# found this source in https://github.com/swirldev/swirl_courses/blob/master/Exploratory_Data_Analysis/CaseStudy/initLesson.R
+path_to_course <- file.path(find.package("swirl"),"Courses/Maps_With_R_Leaflet/Basic_Map/")
+path_to_file <- "capitals.RDS"
+data_file <- paste0(path_to_course, path_to_file)
+
+capitals <- readRDS(data_file)
 linz_lng <- capitals[capitals$GEMEINDE=="Linz", "LNG"]
 linz_lat <- capitals[capitals$GEMEINDE=="Linz", "LAT"]
